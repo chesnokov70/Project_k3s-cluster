685692a68cfd23cdb3c5b2b3f7cfad2feb1ff17e
Sergei Chesnokov <ches.tranveks@gmail.com>
2025-02-12 20:57:28 -0500

    pacman23

--- K8S_Project/pipelines/k3s_cluster_infra_turn_on.groovy
+++ K8S_Project/pipelines/k3s_cluster_infra_turn_on.groovy
@@ -41,13 +41,13 @@ pipeline {
                 curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --yes --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
                 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
                 sudo apt-get update
-                sudo apt-get install -y jq kubeadm kubelet kubectl
+                sudo apt-get install -y kubeadm kubelet kubectl
                 sudo apt-get install -y ansible
                 sudo apt install awscli -y
                  '''
             }
         }
-        stage('Initialize and Plan') {
+        stage('Main Initialize and Plan') {
             steps {
                 dir('K8S_Project/terraform/1-main_setup') {
                     sh '''
@@ -73,11 +73,6 @@ pipeline {
                     sh '''
                         terraform init
                         terraform plan -out=tfplan
-                        k3s_master_instance_public_dns=$(terraform output -raw k3s_master_instance_public_dns)
-                        # Set it as an environment variable
-                        export k3s_master_instance_public_dns="$k3s_master_instance_public_dns"
-                        # Optional: Print the environment variable
-                        echo "The instance public DNS is set to: $k3s_master_instance_public_dns"
                     '''
                 }
             }
@@ -97,6 +92,11 @@ pipeline {
                     sh '''
                         terraform init
                         terraform plan -out=tfplan
+                        k3s_master_instance_public_dns=$(terraform output -raw k3s_master_instance_public_dns)
+                        # Set it as an environment variable
+                        export k3s_master_instance_public_dns="$k3s_master_instance_public_dns"
+                        # Optional: Print the environment variable
+                        echo "The instance public DNS is set to: $k3s_master_instance_public_dns"
                     '''
                 }
             }
@@ -132,8 +132,16 @@ pipeline {
                     }
                 }
             }
-        } 
-        }               
+        }
+        stage('deploy Pacman') {
+            steps {
+                script {
+                    // Trigger another Jenkins pipeline with pacman
+                    build job: 'deploy_pacman_in_to_k3s_cluster',
+                    wait: true
+                }
+            }
+        }
     }
 
     post {
