1afa173b7a20e68b0015d837a08df9b023c21c0f
Sergei Chesnokov <ches.tranveks@gmail.com>
2025-02-13 22:52:13 -0500

    jenkins-5

--- K8S_Project/pipelines/deploy_pacman_in_to_k3s_cluster.groovy
+++ K8S_Project/pipelines/deploy_pacman_in_to_k3s_cluster.groovy
@@ -40,7 +40,7 @@ pipeline {
                 dir('K8S_Project/cluster_applications/pacman/') {
                     script {
                         sh '''
-                        bash update_host_pacman.sh
+                        ./update_host_pacman.sh
                         cat 2-pacman.yaml
                         '''
                     }
